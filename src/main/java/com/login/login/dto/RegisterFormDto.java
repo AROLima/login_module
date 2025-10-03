354d1b60cebe385d6a68879f5515aaeb725ce8f8
Rodrigo Lima <Rodrigo.lima546@hotmail.com>
2025-10-03 05:05:01 -0300

    jwt service created

--- src/main/java/com/login/login/dto/RegisterFormDto.java
+++ src/main/java/com/login/login/dto/RegisterFormDto.java
@@ -3,7 +3,12 @@ package com.login.login.dto;
 import jakarta.validation.constraints.*;
 
 public record RegisterFormDto(
-  @Email @NotBlank String email,
-  @NotBlank @Size(min = 8) String password,
-  @NotBlank String name
+  @Email 
+  @NotBlank 
+  String email,
+  @NotBlank 
+  @Size(min = 8) 
+  String password,
+  @NotBlank 
+  String name
 ) {}
